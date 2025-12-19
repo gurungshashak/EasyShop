b7feb76a90c06fa56e9d6058ffab546042e5c9ce
gurungshashak <gurungshashak@gmail.com>
2025-12-19 08:43:08 -0500

    Shopping cart!

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,8 +1,6 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -14,8 +12,6 @@ import java.security.Principal;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
-@RestController
-@PreAuthorize("users")
 public class ShoppingCartController
 {
     // a shopping cart requires
